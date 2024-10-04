31778e128eacc57dadfffed3d58edda1fbc96b79
Demian <53890358+demian35@users.noreply.github.com>
2024-10-03 19:24:10 -0600

    creando el controlador del api

---
+++ api-rest/src/main/java/com/example/api_rest/controller/EmpleadoController.java
@@ -0,0 +1,13 @@
+package com.example.api_rest.controller;
+import com.example.api_rest.Empleado;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/empleados")
+
+public class EmpleadoController {
+    
+}
