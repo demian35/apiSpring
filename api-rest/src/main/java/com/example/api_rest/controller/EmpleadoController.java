ed06e1f8439224ef022d0aa9f3cb45f27b38787b
Demian <53890358+demian35@users.noreply.github.com>
2024-10-03 19:48:37 -0600

    metodos para obtener todos los datos del api

--- api-rest/src/main/java/com/example/api_rest/controller/EmpleadoController.java
+++ api-rest/src/main/java/com/example/api_rest/controller/EmpleadoController.java
@@ -1,13 +1,43 @@
 package com.example.api_rest.controller;
+
+
 import com.example.api_rest.Empleado;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/empleados")
 
+//clase que funje como el controlador del sistema
 public class EmpleadoController {
+    //creamos una lista de empleados
+    private List<Empleado> empleados= new ArrayList<>();
+
+    //metodo para obtener los empleados
+    @GetMapping("/empleados")
+    public List<Empleado> getAllmpleados(){
+        return empleados;
+    }
+
+    //obtenemos el empleado por su id
+    @GetMapping("/empleados/{id}")
+    public Empleado getEmpleadoById(@PathVariable int id){
+        return empleados.stream().filter(e->e.getidEmpleado()==id).findFirst().orElse(null);
+    }
+
+    //creamos un nuevo empleado
+    @PostMapping("path")
+    public String agregaEmpleado(@RequestBody String nombre) {
+        //TODO: process POST request
+        
+        return entity;
+    }
     
+
+
 }
